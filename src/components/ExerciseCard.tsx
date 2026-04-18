a49f353a29485e0378221edc40d5ee18c4084b61
Tamara <thankgoditemearau@gmail.com>
2026-04-18 23:07:23 +0100

    fix: width of exercise cards

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -9,7 +9,7 @@ const ExerciseCard = ({exercise, addToFavourite}: ExerciseCardProps
 ) => {
   return (
      <div className="bg-(--app-bg) mx-8 mt-6">
-      <div className="bg-white border-2 border-gray-200  hover:border-(--card-hover) rounded-xl p-4 shadow-sm transition flex flex-col gap-1 items-start md:[30] lg:w-[50vh] m-auto">
+      <div className="bg-white border-2 border-gray-200  hover:border-(--card-hover) rounded-xl p-4 shadow-sm transition flex flex-col gap-1 items-start md:[30] lg:w-[40vh] m-auto">
         <h3 className="bg-(--muscle-bg) text-(--accent-text) w-fit px-3 py-1 rounded-full text-xs font-semibold">{exercise.target}</h3>
         <h4 className="text-black text-sm">{exercise.name}</h4>
         <p className="text-gray-400 text-[11px]">{exercise.equipment}</p>
