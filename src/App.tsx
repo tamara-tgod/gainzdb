08d88ce016ee53df08a246b73f7108ec72c4fc55
Tamara <thankgoditemearau@gmail.com>
2026-04-28 19:51:32 +0100

    fix: correct layout of favourites drawer

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ function App() {
   const [bodyPart, setBodyPart] = useState("All");
   const [equipment, setEquipment] = useState("All");
   const { data, loading, error } = useFetchExercise();
-  const [favourite, setFavourite] = useState<Exercise[]>([])
-  const [isOpen, setIsOpen] = useState(false)
+  const [favourite, setFavourite] = useState<Exercise[]>([]);
+  const [isOpen, setIsOpen] = useState(false);
 
   if (loading) return <p>Loading...</p>;
   if (error) {
@@ -21,90 +21,93 @@ function App() {
 
   // filter logic
   const filteredData = data?.filter((exercise) => {
-   
-   if (bodyPart !== "All" && exercise.bodyPart.toLowerCase() !== bodyPart.toLowerCase()) {
-    return false;
-   } 
-   
-   if (equipment !== "All" && exercise.equipment !== equipment) {
-    return false
-   }
+    if (
+      bodyPart !== "All" &&
+      exercise.bodyPart.toLowerCase() !== bodyPart.toLowerCase()
+    ) {
+      return false;
+    }
+
+    if (equipment !== "All" && exercise.equipment !== equipment) {
+    
+      return false;
+    }
 
     return true;
-});
+  });
 
-// bodyparts and equipment array from the api data
-const bodies = data?.map(item => item.bodyPart) ?? [];
-const bodyParts = ["All", ...new Set(bodies)];
+  // bodyparts and equipment array from the api data
+  const bodies = data?.map((item) => item.bodyPart) ?? [];
+  const bodyParts = ["All", ...new Set(bodies)];
 
-const exerciseEquipments = data?.map(equipmentItem => equipmentItem.equipment) ?? [];
-console.log(exerciseEquipments)
-const equipments = ["All", ...new Set(exerciseEquipments)]
+  const exerciseEquipments =
+    data?.map((equipmentItem) => equipmentItem.equipment) ?? [];
+  console.log(exerciseEquipments);
+  const equipments = ["All", ...new Set(exerciseEquipments)];
 
-//add favourites exercises
-const addToFavourite = (exercise: Exercise): void => {
-   setFavourite((prev) => {
-    const existingExercise = prev.some((item) => item.id === exercise.id);
-    if(existingExercise) {
-      return prev;
-    }
-    console.log([...prev, exercise])
-     return [...prev, exercise]
-     
-     } )
-}
+  //add favourites exercises
+  const addToFavourite = (exercise: Exercise): void => {
+    setFavourite((prev) => {
+      const existingExercise = prev.some((item) => item.id === exercise.id);
+      if (existingExercise) {
+        return prev;
+      }
+      console.log([...prev, exercise]);
+      return [...prev, exercise];
+    });
+  };
 
-// handle remove button/event for FavouriteCard
-const removeFromFavourite = (exercise: Exercise): void => {
-   setFavourite((prev) => {
-    return prev.filter((item) => item.id !== exercise.id);
-     
-     } )
-}
+  // handle remove button/event for FavouriteCard
+  const removeFromFavourite = (exercise: Exercise): void => {
+    setFavourite((prev) => {
+      return prev.filter((item) => item.id !== exercise.id);
+    });
+  };
 
-// button/event handlers for save, toolbar and favouriteDrawer
- const onSavedClick = (): void => {
-        setIsOpen(prev => !prev)
-    }
+  // button/event handlers for save, toolbar and favouriteDrawer
+  const onSavedClick = (): void => {
+    setIsOpen((prev) => !prev);
+  };
 
-    const handleClose = () => {
-  setIsOpen(false);
-};
-
+  const handleClose = () => {
+    setIsOpen(false);
+  };
 
   return (
     <div>
-      <Toolbar onSavedClick={onSavedClick}
-      favouritesCount={favourite.length} />
-      <FavouritesDrawer 
-      favourites={favourite}
-      isOpen={isOpen}
-      onClose={handleClose}
-      removeFromFavourite={removeFromFavourite}
-       />
-      <div 
-      
-      className="min-h-screen py-6 flex flex-col md:flex-row gap-6">
-   <div className=" px-3 py-2 bg-white">
-       <Sidebar
-      bodyPart={bodyPart}
-      equipment={equipment}
-      handleBodyPartFilter = {setBodyPart}
-      handleEquipmentFilter={setEquipment}
-      bodyParts={bodyParts}
-      equipments={equipments}
-       />
-   </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 bg-(--app-bg) ">
-        {filteredData?.map((exercise) => (
-          <ExerciseCard key={exercise.id} exercise={exercise} 
-          addToFavourite={addToFavourite}
+      <Toolbar onSavedClick={onSavedClick} favouritesCount={favourite.length} />
+   
+          <FavouritesDrawer
+        favourites={favourite}
+        isOpen={isOpen}
+        onClose={handleClose}
+        removeFromFavourite={removeFromFavourite}
+        
+      />
+      <div className="min-h-screen py-6 flex flex-col md:flex-row gap-6">
+        <div className=" px-3 py-2 bg-white">
+          <Sidebar
+            bodyPart={bodyPart}
+            equipment={equipment}
+            handleBodyPartFilter={setBodyPart}
+            handleEquipmentFilter={setEquipment}
+            bodyParts={bodyParts}
+            equipments={equipments}
           />
-        ))}
+        </div>
+        <div className="grid grid-cols-1  lg:flex flex-wrap gap-3 bg-(--app-bg) ">
+          {filteredData?.map((exercise) => (
+            <ExerciseCard
+              key={exercise.id}
+              exercise={exercise}
+              addToFavourite={addToFavourite}
+            />
+          ))}
+        </div>
       </div>
-    </div>
     </div>
   );
 }
 
 export default App;
+//grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3
--- src/components/FavouritesDrawer.tsx
+++ src/components/FavouritesDrawer.tsx
@@ -2,29 +2,37 @@ import type { Exercise } from "../types/exercise";
 import FavouriteCard from "./FavouriteCard";
 
 interface FavouriteProps {
-    favourites: Exercise[];
-    isOpen: boolean;
-    onClose: () => void;
-    removeFromFavourite: (value: Exercise) => void;
+  favourites: Exercise[];
+  isOpen: boolean;
+  onClose: () => void;
+  removeFromFavourite: (value: Exercise) => void;
 }
 
-const FavouritesDrawer = ({favourites, isOpen, onClose, removeFromFavourite}: FavouriteProps) => {
-    if (!isOpen) return null;
+const FavouritesDrawer = ({
+  favourites,
+  isOpen,
+  onClose,
+  removeFromFavourite,
+}: FavouriteProps) => {
+  if (!isOpen) return null;
 
   return (
-    <div>
-    <button onClick={onClose}>Close</button>
- <div>
-    {favourites.map((favourite) => (
-        <div
-        key={favourite.id}
-        >
-            <FavouriteCard exercise={favourite}  removeFromFavourite={removeFromFavourite}/>
-        </div>
-    ))}
- </div>
+    <div className="bg-(--app-bg) fixed right-0 top-0 h-full w-full  border-2 border-green-400 overflow-y-auto">
+       <button onClick={onClose} className="text-end">
+          Close
+        </button>
+      <div className="flex flex-col md:flex-row md:flex-wrap">
+         {favourites.map((favourite) => (
+          <div key={favourite.id}>
+            <FavouriteCard
+              exercise={favourite}
+              removeFromFavourite={removeFromFavourite}
+            />
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FavouritesDrawer
+export default FavouritesDrawer;
